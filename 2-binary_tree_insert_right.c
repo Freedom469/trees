fa736531f859c31dbb876ef74d5d757b117f2971
Malik <bamjnr99@gmail.com>
2023-03-01 22:32:26 -0800

    a function that inserts a node as the right-child of another node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent->right == NULL)
 	{
-		binary_tree_node(parent, value);
+		parent->rignt = binary_tree_node(parent, value);
 	}
 
 	else
