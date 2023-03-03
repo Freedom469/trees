566e0c59ca30a41c35b52c52e74701d28352e7e6
Malik <bamjnr99@gmail.com>
2023-03-02 18:11:46 -0800

    fixed betty style

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+  * binary_tree_height - measures the height of a binary tree
+  * @tree: pointer to the root node of the tree to measure the height
+  *
+  * Return: height of the tree
+  *
+  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -21,12 +28,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (height_left <  height_right)
 	{
-		return (1 +( height_right));
+		return (1 + (height_right));
 	}
 
 	else
 	{
-		return (1 +( height_left));
+		return (1 + (height_left));
 	}
 
 }
